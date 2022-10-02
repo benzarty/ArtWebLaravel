9c06424bdf4b06f250c2dc67b8b26b7b19f1d823
mounaayed <mouna.ayed@esprit.tn>
2022-10-02 14:00:22 +0200

    mouna

---
+++ resources/views/byccle.blade.php
@@ -0,0 +1,412 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from www.codezion.com/themes/tm/html/bicycle/bicycle.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 26 Sep 2022 02:56:37 GMT -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta name="keywords" content="#">
+<meta name="description" content="#">
+<title>Bicycle Bootstrap Website Templates  | Bicycle</title>
+
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{asset('/images/favicon.ico')}}">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{asset('/images/favicon.ico')}}">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{asset('/images/favicon.ico')}}">
+<link rel="apple-touch-icon-precomposed" href="{{asset('/images/favicon.ico')}}">
+<link rel="shortcut icon" href="{{asset('/images/favicon.ico')}}">
+
+<link href="{{asset('/css/bootstrap.min.css')}}" rel="stylesheet">
+
+<link href="{{asset('/css/font-awesome.css')}}" rel="stylesheet">
+
+<link href="{{asset('/css/font/flaticon.css')}}" rel="stylesheet">
+
+<link href="{{asset('/css/slick.css')}}" rel="stylesheet">
+
+<link href="{{asset('/css/ion.rangeSlider.min.css')}}" rel="stylesheet">
+
+<link href="{{asset('/css/datepicker.css')}}" rel="stylesheet">
+
+<link href="{{asset('/css/magnific-popup.css')}}" rel="stylesheet">
+
+<link href="{{asset('/css/nice-select.css')}}" rel="stylesheet">
+
+<link href="{{asset('/css/style.css')}}" rel="stylesheet">
+
+<link href="{{asset('/css/responsive.css')}}" rel="stylesheet">
+<link href="{{asset('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&amp;display=swap')}}" rel="stylesheet">
+<link href="{{asset('https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&amp;display=swap')}}" rel="stylesheet">
+
+<!--[if lte IE 9]>
+        <link rel="stylesheet" type="text/css" href="css/ie.css" />
+    <![endif]-->
+
+<!--[if lt IE 9]>
+      <script type='text/javascript' src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+      <script type='text/javascript' src="http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+
+
+<header class="header">
+<div class="topbar bg-theme">
+<div class="container">
+<div class="row">
+<div class="col-lg-6 col-md-5">
+<div class="leftside">
+<ul class="custom-flex">
+<li> <a href="#" class="text-custom-white"><i class="fab fa-facebook-f"></i></a> </li>
+<li> <a href="#" class="text-custom-white"><i class="fab fa-twitter"></i></a> </li>
+<li> <a href="#" class="text-custom-white"><i class="fab fa-instagram"></i></a> </li>
+<li> <a href="#" class="text-custom-white"><i class="fab fa-linkedin"></i></a> </li>
+</ul>
+</div>
+</div>
+<div class="col-lg-6 col-md-7">
+<div class="rightside full-height">
+<ul class="custom-flex full-height">
+<li class="book-appointment"> <a href="booking.html"> Booking Now </a> </li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="navigation-wrapper">
+<div class="container">
+<div class="row">
+<div class="col-12">
+<nav>
+<div class="main-navigation">
+<div class="logo"> <a href="index.html"> <img src="{{asset('/images/logo.png')}}" class="img-fluid" alt="logo"> </a> </div>
+<div class="main-menu">
+<ul class="custom-flex">
+<li class="menu-item active"> <a href="index.html">Home</a> </li>
+<li class="menu-item menu-item-has-children"> <a href="#">Inventory</a>
+<ul class="submenu custom">
+<li class="menu-item"> <a href="bicycle.html">Full Width</a> </li>
+<li class="menu-item"> <a href="bicycle-left-sidebar.html">Left Sidebar</a> </li>
+<li class="menu-item"> <a href="bicycle-right-sidebar.html">Right Sidebar</a> </li>
+<li class="menu-item"> <a href="bicycle-detail.html">Bicycle Detail</a> </li>
+</ul>
+</li>
+<li class="menu-item menu-item-has-children"> <a href="#">Blog</a>
+<ul class="submenu custom">
+<li class="menu-item"> <a href="blog.html">Blog Grid</a> </li>
+<li class="menu-item"> <a href="blog-left-sidebar.html">Blog Left</a> </li>
+<li class="menu-item"> <a href="blog-right-sidebar.html">Blog right</a> </li>
+<li class="menu-item"> <a href="blog-details.html">Blog Detail</a> </li>
+<li class="menu-item"> <a href="blog-details-left-sidebar.html">Blog Detail Left</a> </li>
+<li class="menu-item"> <a href="blog-details-right-sidebar.html">Blog Detail Right</a> </li>
+</ul>
+</li>
+<li class="desk-logo">
+<div class="logo"> <a href="index.html"> <img src="{{asset('/images/logo.png')}}" class="img-fluid" alt="logo"> </a> </div>
+</li>
+<li class="menu-item"> <a href="about.html">About</a> </li>
+<li class="menu-item menu-item-has-children"> <a href="#">Pages</a>
+<ul class="submenu custom">
+<li class="menu-item"> <a href="booking.html">Booking</a> </li>
+<li class="menu-item"> <a href="faqs.html">Faq</a> </li>
+<li class="menu-item"> <a href="gallery.html">Gallery</a> </li>
+<li class="menu-item"> <a href="404.html">404</a> </li>
+<li class="menu-item"> <a href="coming-soon.html">Coming Soon</a> </li>
+</ul>
+</li>
+<li class="menu-item"> <a href="contact.html">Contact</a> </li>
+</ul>
+</div>
+<div class="hamburger-menu">
+<div class="menu-btn"> <span></span> <span></span> <span></span> </div>
+</div>
+</div>
+</nav>
+</div>
+</div>
+</div>
+</div>
+</header>
+
+
+
+<div class="subheader normal-bg section-padding">
+<div class="container">
+<div class="row">
+<div class="col-12 text-left">
+<h1 class="text-custom-white">Bicycle</h1>
+<ul class="custom-flex">
+<li class="fw-500"> <a href="index.html" class="text-custom-white">Home</a> </li>
+<li class="active fw-500"> Bicycle </li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+
+
+<section class="section-padding">
+<div class="container">
+<div class="row">
+<div class="col-12">
+<div class="listing-top-heading mb-xl-20">
+<h6 class="no-margin text-custom-black">Showing 8 Results</h6>
+<div class="sort-by"> <span class="text-custom-black fs-14 fw-600">Sort by</span>
+<div class="group-form">
+<select class="form-control form-control-custom custom-select">
+<option>A to Z</option>
+<option>Z to A</option>
+</select>
+</div>
+</div>
+</div>
+</div>
+<div class=" col-lg-4 col-md-6">
+<div class="bicycle-grid mb-xl-30">
+<div class="bicycle-grid-wrapper bicycle-grid bx-wrapper">
+<div class="image-sec animate-img"> <a href="#"> <img src="{{asset('/images/bicycle/cy-2.jpg')}}" class="full-width" alt="img"> </a> </div>
+<div class="bicycle-grid-caption padding-20 bg-custom-white p-relative">
+<h4 class="title fs-16"><a href="#" class="text-custom-black">Road Bicycle<small class="text-light-dark">Per Day</small></a></h4>
+<span class="price"><small>From</small>$18</span>
+<div class="action"> <a class="btn-second btn-small" href="#">View</a> <a class="btn-first btn-submit" href="#">Book</a> </div>
+</div>
+</div>
+</div>
+</div>
+<div class=" col-lg-4 col-md-6">
+<div class="bicycle-grid mb-xl-30">
+<div class="bicycle-grid-wrapper bicycle-grid bx-wrapper">
+<div class="image-sec animate-img"> <a href="#"> <img src="{{asset('/images/bicycle/cy-3.jpg')}}" class="full-width" alt="img"> </a> </div>
+<div class="bicycle-grid-caption padding-20 bg-custom-white p-relative">
+<h4 class="title fs-16"><a href="#" class="text-custom-black">Road Bicycle<small class="text-light-dark">2 Day</small></a></h4>
+<span class="price"><small>From</small>$58</span>
+<div class="action"> <a class="btn-second btn-small" href="#">View</a> <a class="btn-first btn-submit" href="#">Book</a> </div>
+</div>
+</div>
+</div>
+</div>
+<div class=" col-lg-4 col-md-6">
+<div class="bicycle-grid mb-xl-30">
+<div class="bicycle-grid-wrapper bicycle-grid bx-wrapper">
+<div class="image-sec animate-img"> <a href="#"> <img src="{{asset('/images/bicycle/cy-2.jpg')}}" class="full-width" alt="img"> </a> </div>
+<div class="bicycle-grid-caption padding-20 bg-custom-white p-relative">
+<h4 class="title fs-16"><a href="#" class="text-custom-black">Road Bicycle<small class="text-light-dark">Per Day</small></a></h4>
+<span class="price"><small>From</small>$28</span>
+<div class="action"> <a class="btn-second btn-small" href="#">View</a> <a class="btn-first btn-submit" href="#">Book</a> </div>
+</div>
+</div>
+</div>
+</div>
+<div class=" col-lg-4 col-md-6">
+<div class="bicycle-grid mb-xl-30">
+<div class="bicycle-grid-wrapper bicycle-grid bx-wrapper">
+<div class="image-sec animate-img"> <a href="#"> <img src="{{asset('/images/bicycle/cy-3.jpg')}}" class="full-width" alt="img"> </a> </div>
+<div class="bicycle-grid-caption padding-20 bg-custom-white p-relative">
+<h4 class="title fs-16"><a href="#" class="text-custom-black">Road Bicycle<small class="text-light-dark">Per Day</small></a></h4>
+<span class="price"><small>From</small>$58</span>
+<div class="action"> <a class="btn-second btn-small" href="#">View</a> <a class="btn-first btn-submit" href="#">Book</a> </div>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6">
+<div class="bicycle-grid mb-xl-30">
+<div class="bicycle-grid-wrapper bicycle-grid bx-wrapper">
+<div class="image-sec animate-img"> <a href="#"> <img src="{{asset('/images/bicycle/cy-2.jpg')}}" class="full-width" alt="img"> </a> </div>
+<div class="bicycle-grid-caption padding-20 bg-custom-white p-relative">
+<h4 class="title fs-16"><a href="#" class="text-custom-black">Road Bicycle<small class="text-light-dark">Per Day</small></a></h4>
+<span class="price"><small>From</small>$58</span>
+<div class="action"> <a class="btn-second btn-small" href="#">View</a> <a class="btn-first btn-submit" href="#">Book</a> </div>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6">
+<div class="bicycle-grid mb-xl-30">
+<div class="bicycle-grid-wrapper bicycle-grid bx-wrapper">
+<div class="image-sec animate-img"> <a href="#"> <img src="{{asset('/images/bicycle/cy-3.jpg')}}" class="full-width" alt="img"> </a> </div>
+<div class="bicycle-grid-caption padding-20 bg-custom-white p-relative">
+<h4 class="title fs-16"><a href="#" class="text-custom-black">Road Bicycle<small class="text-light-dark">Per Day</small></a></h4>
+<span class="price"><small>From</small>$158</span>
+<div class="action"> <a class="btn-second btn-small" href="#">View</a> <a class="btn-first btn-submit" href="#">Book</a> </div>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6">
+<div class="bicycle-grid mb-xl-30">
+<div class="bicycle-grid-wrapper bicycle-grid bx-wrapper">
+<div class="image-sec animate-img"> <a href="#"> <img src="{{asset('/images/bicycle/cy-2.jpg')}}" class="full-width" alt="img"> </a> </div>
+<div class="bicycle-grid-caption padding-20 bg-custom-white p-relative">
+<h4 class="title fs-16"><a href="#" class="text-custom-black">Road Bicycle<small class="text-light-dark">Per Day</small></a></h4>
+<span class="price"><small>From</small>$58</span>
+<div class="action"> <a class="btn-second btn-small" href="#">View</a> <a class="btn-first btn-submit" href="#">Book</a> </div>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6">
+<div class="bicycle-grid mb-xl-30">
+<div class="bicycle-grid-wrapper bicycle-grid bx-wrapper">
+<div class="image-sec animate-img"> <a href="#"> <img src="{{asset('/images/bicycle/cy-3.jpg')}}" class="full-width" alt="img"> </a> </div>
+<div class="bicycle-grid-caption padding-20 bg-custom-white p-relative">
+<h4 class="title fs-16"><a href="#" class="text-custom-black">Road Bicycle<small class="text-light-dark">Per Day</small></a></h4>
+<span class="price"><small>From</small>$58</span>
+<div class="action"> <a class="btn-second btn-small" href="#">View</a> <a class="btn-first btn-submit" href="#">Book</a> </div>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6">
+<div class="bicycle-grid mb-md-30">
+<div class="bicycle-grid-wrapper bicycle-grid bx-wrapper">
+<div class="image-sec animate-img"> <a href="#"> <img src="{{asset('/images/bicycle/cy-2.jpg')}}" class="full-width" alt="img"> </a> </div>
+<div class="bicycle-grid-caption padding-20 bg-custom-white p-relative">
+<h4 class="title fs-16"><a href="#" class="text-custom-black">Road Bicycle<small class="text-light-dark">Per Day</small></a></h4>
+<span class="price"><small>From</small>$58</span>
+<div class="action"> <a class="btn-second btn-small" href="#">View</a> <a class="btn-first btn-submit" href="#">Book</a> </div>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6">
+<div class="bicycle-grid mb-xl-30">
+<div class="bicycle-grid-wrapper bicycle-grid bx-wrapper">
+<div class="image-sec animate-img"> <a href="#"> <img src="{{asset('/images/bicycle/cy-3.jpg')}}" class="full-width" alt="img"> </a> </div>
+<div class="bicycle-grid-caption padding-20 bg-custom-white p-relative">
+<h4 class="title fs-16"><a href="#" class="text-custom-black">Road Bicycle<small class="text-light-dark">Per Day</small></a></h4>
+<span class="price"><small>From</small>$58</span>
+<div class="action"> <a class="btn-second btn-small" href="#">View</a> <a class="btn-first btn-submit" href="#">Book</a> </div>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6">
+<div class="bicycle-grid mb-xl-30">
+<div class="bicycle-grid-wrapper bicycle-grid bx-wrapper">
+<div class="image-sec animate-img"> <a href="#"> <img src="{{asset('/images/bicycle/cy-2.jpg')}}" class="full-width" alt="img"> </a> </div>
+<div class="bicycle-grid-caption padding-20 bg-custom-white p-relative">
+<h4 class="title fs-16"><a href="#" class="text-custom-black">Road Bicycle<small class="text-light-dark">Per Day</small></a></h4>
+<span class="price"><small>From</small>$58</span>
+<div class="action"> <a class="btn-second btn-small" href="#">View</a> <a class="btn-first btn-submit" href="#">Book</a> </div>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6">
+<div class="bicycle-grid mb-xl-30">
+<div class="bicycle-grid-wrapper bicycle-grid bx-wrapper">
+<div class="image-sec animate-img"> <a href="#"> <img src="{{asset('/images/bicycle/cy-3.jpg')}}" class="full-width" alt="img"> </a> </div>
+<div class="bicycle-grid-caption padding-20 bg-custom-white p-relative">
+<h4 class="title fs-16"><a href="#" class="text-custom-black">Road Bicycle<small class="text-light-dark">2 Day</small></a></h4>
+<span class="price"><small>From</small>$18</span>
+<div class="action"> <a class="btn-second btn-small" href="#">View</a> <a class="btn-first btn-submit" href="#">Book</a> </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="col-12">
+<nav>
+<ul class="pagination justify-content-center">
+<li class="page-item"><a class="page-link" href="#">Previous</a></li>
+<li class="page-item"><a class="page-link" href="#">1</a></li>
+<li class="page-item active"><a class="page-link" href="#">2</a></li>
+<li class="page-item"><a class="page-link" href="#">3</a></li>
+<li class="page-item"><a class="page-link" href="#">Next</a></li>
+</ul>
+</nav>
+</div>
+</div>
+</div>
+</section>
+
+
+<footer class="section-padding footer">
+<div class="container">
+<div class="row">
+<div class="col-lg-4 col-md-6">
+<div class="footer-box mb-md-40">
+<h4 class="text-custom-white fw-600">About Us</h4>
+<p class="text-custom-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.Lorem ipsum dolor sit amet... </p>
+</div>
+</div>
+<div class="col-lg-4 col-md-6">
+<div class="footer-box mb-md-40">
+<h4 class="text-custom-white fw-600">About Us</h4>
+<ul class="m-0 p-0 main">
+<li>1-567-124-44227</li>
+<li>182 main street pert habour 8007</li>
+<li>Mon-sat 8:00-18:00 Sunday Closed</li>
+</ul>
+<ul class="custom-flex socials">
+<li><a href="#" class="text-custom-white fs-14"><i class="fab fa-facebook-f"></i></a></li>
+<li><a href="#" class="text-custom-white fs-14"><i class="fab fa-twitter"></i></a></li>
+<li><a href="#" class="text-custom-white fs-14"><i class="fab fa-linkedin"></i></a></li>
+<li><a href="#" class="text-custom-white fs-14"><i class="fab fa-youtube"></i></a></li>
+</ul>
+</div>
+</div>
+<div class="col-lg-4 col-md-6">
+<div class="footer-box mb-sm-40">
+<h4 class="text-custom-white fw-600">Newsletter</h4>
+<div class="newsletter">
+<p class="text-custom-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+<form>
+<div class="form-group">
+<input type="email" name="#" class="form-control form-control-custom" placeholder="Email I'd">
+</div>
+<button type="submit" class="btn-first btn-submits">Subscribe</button>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
+</footer>
+
+
+<div class="copyright">
+<div class="container">
+<div class="row">
+<div class="col-12">
+<p class="text-custom-white">© Bicycle - 2020 | All Right Reserved.</p>
+</div>
+</div>
+</div>
+</div>
+
+<div id="back-top" class="back-top"> <a href="#top"><i class="flaticon-arrows"></i></a> </div>
+
+
+<script src="{{asset('/js/jquery.min.js')}}"></script>
+
+<script src="{{asset('/js/popper.min.js')}}"></script>
+
+<script src="{{asset('/js/bootstrap.min.js')}}"></script>
+
+<script src="{{asset('/js/ion.rangeSlider.min.js')}}"></script>
+
+<script src="{{asset('/js/slick.min.js')}}"></script>
+
+<script src="{{asset('/js/datepicker.js')}}"></script>
+<script src="{{asset('/js/datepicker.en.js')}}"></script>
+
+<script src="{{asset('/js/isotope.pkgd.min.js')}}"></script>
+
+<script src="{{asset('/js/jquery.nice-select.js')}}"></script>
+
+<script src="{{asset('/js/jquery.magnific-popup.min.js')}}"></script>
+
+<script src="{{asset('https://maps.googleapis.com/maps/api/js?key=AIzaSyDnd9JwZvXty-1gHZihMoFhJtCXmHfeRQg')}}"></script>
+
+<script src="{{asset('/js/custom.js')}}"></script>
+
+</body>
+
+<!-- Mirrored from www.codezion.com/themes/tm/html/bicycle/bicycle.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 26 Sep 2022 02:56:37 GMT -->
+</html>
